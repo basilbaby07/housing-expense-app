5cfa5e1b8fb85b06b9c7f80faee5b5d66152ffb0
basilbaby07 <BASILBABY07@YAHOO.COM>
2025-06-09 21:28:36 +1200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import FinancialTableGenerator from './FinancialTableGenerator';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FinancialTableGenerator />
+  </React.StrictMode>
+);
